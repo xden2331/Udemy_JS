e4af69e121d46014fe67367a5d543d4debe2f32c
RicXD <291261411@qq.com>
2019-05-16 16:59:28 +1000

    Finish section 3, lecture 10

---
+++ basics/section3_lecture10_challenge.js
@@ -0,0 +1,5 @@
+let city = "Sydney"
+let country = "Australia"
+let location = city + ", " + country
+
+console.log(location)
---
+++ basics/strings.js
@@ -0,0 +1,5 @@
+let firstName = 'Xing'
+let lastName = 'DENG'
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
